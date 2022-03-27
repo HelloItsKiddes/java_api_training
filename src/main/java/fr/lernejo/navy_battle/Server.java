27e927249419f9c868263411203dd57db3aed02e
HelloItsKiddes <aboubekre.sayoud@gmail.com>
2022-03-27 19:12:18 +0200

    Test addes to git

--- src/main/java/fr/lernejo/navy_battle/Server.java
+++ src/main/java/fr/lernejo/navy_battle/Server.java
@@ -12,7 +12,7 @@ public class Server {
 
     final HttpServer server;
 
-    Server(int port) throws IOException {
+    public Server(int port) throws IOException {
         this.server = HttpServer.create(new InetSocketAddress(port), 0);
         this.server.setExecutor(Executors.newSingleThreadExecutor());
         this.server.createContext("/api/game/start", new StartGame(port));
